2953a1d7bd8e9d67082c127503c85c55e417f681
felixstahmer <felix.stahmer@t-online.de>
2022-02-10 13:38:57 +0100

    improve file structure for active game

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import sys
 
 from src.draw.draw_result import draw_result
 
-from src.game_elements.node.node import Node
-from src.game_elements.node.node_list import NodeList
-from src.vc.vc_controller import VisualComputingController
 from src.game.game_controller import GameController
 from src.detection.detection_controller import DetectionController
 from src.directory.create_directory import FileManager
@@ -14,52 +11,65 @@ from src.minizinc.minizinc_controller import MinizincController
 
 def main():
     base_dir = "activegame"
-    file_manager = FileManager(base_dir)
     
-    AMOUNT_OF_LEVELS = 50
+    AMOUNT_OF_WORLDS = 2
+    AMOUNT_OF_LEVELS_WORLD1 = 33
+    AMOUNT_OF_LEVELS_WORLD2 = 50
 
-    level_directories = file_manager.create_directories_for_active_game(AMOUNT_OF_LEVELS)
+    LEVELS_PER_WORLD = [AMOUNT_OF_LEVELS_WORLD1, AMOUNT_OF_LEVELS_WORLD2]
+  
     game_controller = GameController()
-    # level_directories = [1]
+
+    for world in range(AMOUNT_OF_WORLDS):
+        AMOUNT_OF_LEVELS = LEVELS_PER_WORLD[world]
+        world_index = world + 1
+
+        world_dir = "{}/world{}".format(base_dir, world_index)
+
+        file_manager = FileManager(world_dir)
+        file_manager.create_world_directory(world_dir)
+        level_directories = file_manager.create_level_directories(AMOUNT_OF_LEVELS)
     
-    for index, level_directory in enumerate(level_directories): 
-        level_index = index + 1
+        for index, level_directory in enumerate(level_directories): 
+            level_index = index + 1
 
-        print("Calculating for Level {}!".format(level_index))
-        
-        screenshot_dir = "{}/Level{}.png".format(level_directory, level_index)
-        # level_directory = "activegame/Level11"
-        # screenshot_dir = "activegame/Level11/Level11.png"
-        game_controller.take_screenshot(screenshot_dir)
+            print("Calculating World {} Level {}!".format(world_index, level_index))
+            
+            screenshot_dir = "{}/Level{}.png".format(level_directory, level_index)
+            # level_directory = "activegame/Level11"
+            # screenshot_dir = "activegame/Level11/Level11.png"
+            game_controller.take_screenshot(screenshot_dir)
 
-        detection_controller = DetectionController()
-        node_list = detection_controller.detect_nodes(screenshot_dir, level_directory)
-        line_list = detection_controller.detect_lines(screenshot_dir, level_directory, node_list)
+            detection_controller = DetectionController()
+            node_list = detection_controller.detect_nodes(screenshot_dir, level_directory)
+            line_list = detection_controller.detect_lines(screenshot_dir, level_directory, node_list)
 
-        connection_list = detection_controller.detect_connections(level_directory, node_list, line_list)
-        connection_list.find_unique_connections()
+            connection_list = detection_controller.detect_connections(level_directory, node_list, line_list)
+            connection_list.find_unique_connections()
 
-        draw_result(connection_list, screenshot_dir, level_directory)
+            draw_result(connection_list, screenshot_dir, level_directory)
 
-        connection_list.calculate_constraints()
+            connection_list.calculate_constraints()
 
-        minizinc_controller = MinizincController()
-        minizinc_controller.execute_minizinc(node_list)
+            minizinc_controller = MinizincController()
+            minizinc_controller.execute_minizinc(node_list)
 
-        game_controller.play(node_list)
-        
-        if level_index == 33:
-            next_world_url = "https://thedollargame.io/game/level/100/200/1"
-            game_controller.go_to_next_world(next_world_url)
-        else: 
-            game_controller.click_next_level_button()
+            game_controller.play(node_list)
+            
+            if level_index < AMOUNT_OF_LEVELS:
+                game_controller.click_next_level_button()
+            else: 
+                # Jump to next world!
+                next_world_url = "https://thedollargame.io/game/level/100/{}00/1".format(world_index + 1)
+                game_controller.go_to_next_world(next_world_url)
 
-def check_single_level(level_number):
-    base_dir = "activegame"
+
+def check_single_level(world_number, level_number):
+    base_dir = "activegame/world{}".format(world_number)
     level_name = "Level{}".format(level_number)
     screenshot_dir = "{}/{}/{}.png".format(base_dir, level_name, level_name)
     level_directory = "{}/{}".format(base_dir, level_name)
-    # screenshot_dir = "activegame/Level11/Level11.png"
+    
 
     detection_controller = DetectionController()
     node_list = detection_controller.detect_nodes(screenshot_dir, level_directory)
@@ -76,9 +86,10 @@ def check_single_level(level_number):
     minizinc_controller.execute_minizinc(node_list)
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        level_number = int(sys.argv[1])
-        check_single_level(level_number)
+    if len(sys.argv) > 2:
+        world_number = int(sys.argv[1])
+        level_number = int(sys.argv[2])
+        check_single_level(world_number, level_number)
     else:
         main()
 
--- src/directory/create_directory.py
+++ src/directory/create_directory.py
@@ -9,7 +9,7 @@ class FileManager():
         os.makedirs(url, exist_ok=True)
         os.makedirs(node_directory, exist_ok=True)
 
-    def create_directories_for_active_game(self, amount_of_level):
+    def create_level_directories(self, amount_of_level):
         level_directories = []
         for x in range(amount_of_level):
             file_index = x + 1
@@ -21,6 +21,10 @@ class FileManager():
             level_directories.append(directory_url)
 
         return level_directories
+
+    def create_world_directory(self, world_dir):
+        os.makedirs(world_dir, exist_ok=True)
+        # os.makedirs(node_directory, exist_ok=True)
 
 
 
